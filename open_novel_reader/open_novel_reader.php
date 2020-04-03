968a811fc685a86693e8870c582eb15c1068c8e3
nihesato-san <nihei.db@gmail.com>
2020-04-03 21:04:06 +0900

    feat：プラグイン用のファイル追加

---
+++ open_novel_reader/open_novel_reader.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @package open_novel_reader
+ * @version 0.0.1
+ */
+/*
+Plugin Name: Open Novel Reader
+Plugin URI: https://github.com/nihesato-san/OpenNovelForWordPress
+Description: Web小説のオープンソース運用を補助するためのWordPress用Plugin
+Author: @nihesato-san
+Version: 0.0.1
+Author URI: http://www.○○○.com
+*/
+function open_novel_reader()
+{
+    return "Hello Open Novel Reader!!";
+}
+
